9f4c928eb50f0714ab474dcbc216fbd555b12f42
cristloha <cristloha2205@gmail.com>
2022-09-30 10:21:21 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/dukcapil/halbar/pemohon_dukcapil_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dukcapil.halbar.pemohon_dukcapil_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+}
