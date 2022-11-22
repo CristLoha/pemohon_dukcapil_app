01469663b38e0cdbe1b5c1ac8736a90c279c231a
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2022-11-22 16:08:15 +0800

    update

--- lib/app/modules/detailRiwayat/views/detail_riwayat_view.dart
+++ lib/app/modules/detailRiwayat/views/detail_riwayat_view.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:pemohon_dukcapil_app/app/shared/theme.dart';
 
 import '../controllers/detail_riwayat_controller.dart';
 
@@ -19,18 +20,68 @@ class DetailRiwayatView extends GetView<DetailRiwayatController> {
             if (snapshot.connectionState == ConnectionState.done) {
               var data = snapshot.data!.data() as Map<String, dynamic>;
               controller.namaC.text = data['kategori'];
-              return ListView(
-                padding: EdgeInsets.all(20),
-                children: [
-                  Container(
-                    width: 90,
-                    height: Get.height,
-                    decoration: BoxDecoration(color: Colors),
-                    child: Column(
-                      children: [],
+              return Container(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 20,
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 13,
+                      child: Container(
+                        width: Get.width * 0.9,
+                        height: 420,
+                        decoration: BoxDecoration(
+                          color: kWhiteColor,
+                          borderRadius: BorderRadius.circular(4),
+                          border: Border.all(
+                            width: 1,
+                            color: kGreyColor,
+                          ),
+                        ),
+                        child: Container(
+                            padding: EdgeInsets.only(
+                              top: 20,
+                              left: 20,
+                              right: 20,
+                              bottom: 20,
+                            ),
+                            child: Container(
+                                width: Get.width * 0.7,
+                                height: 420,
+                                decoration: BoxDecoration(
+                                  color: kWhiteColor,
+                                  borderRadius: BorderRadius.circular(4),
+                                  border: Border.all(
+                                    width: 1,
+                                    color: kGreyColor,
+                                  ),
+                                ))),
+                      ),
                     ),
-                  ),
-                ],
+                    Positioned(
+                      left: 15,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: kPrimaryColor,
+                          borderRadius: BorderRadius.circular(4),
+                          border: Border.all(
+                            color: kGreyColor,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(2),
+                          child: Text(
+                            'Perekaman E-KTP',
+                            style: whiteTextStyle.copyWith(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               );
             }
 
