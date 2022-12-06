c51ff78a7694192b5c7b04690b6dc4c118e877ee
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2022-12-06 09:50:21 +0800

    update

--- lib/app/modules/akta_perceraian/views/akta_perceraian_view.dart
+++ lib/app/modules/akta_perceraian/views/akta_perceraian_view.dart
@@ -432,6 +432,151 @@ class AktaPerceraianView extends GetView<AktaPerceraianController> {
                   ),
                 ),
               ),
+
+              /// KK
+              Text(
+                'KK',
+                style: blackTextStyle.copyWith(),
+              ),
+              SizedBox(height: 12.h),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.only(left: 15, top: 20, right: 10),
+                  width: 315.w,
+                  height: 140.h,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: kGreyColor,
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      GetBuilder<AktaPerceraianController>(
+                        builder: (c) => c.pickedImageKK != null
+                            ? Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      c.pickedImageKK!.name,
+                                      style: blackTextStyle.copyWith(),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () => c.resetImageKK(),
+                                    child: Icon(
+                                      Icons.delete,
+                                      color: kRedColor,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            : Text(
+                                '*Maks 5 Mb',
+                                style: redTextStyle.copyWith(),
+                              ),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            width: 120.w,
+                            height: 40.h,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(7),
+                            ),
+                            child: GetBuilder<AktaPerceraianController>(
+                              builder: (c) {
+                                return c.pickedImageKK != null
+                                    ? ElevatedButton(
+                                        onPressed: () {
+                                          Get.dialog(
+                                            Container(
+                                              child: PhotoView(
+                                                imageProvider: FileImage(
+                                                  File(c.pickedImageKK!.path),
+                                                ),
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                        child: Text(
+                                          'Lihat',
+                                          style: blackTextStyle.copyWith(
+                                            fontSize: 16.sp,
+                                            fontWeight: medium,
+                                          ),
+                                        ),
+                                        style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(7),
+                                            side: BorderSide(
+                                              color: kGreyColor,
+                                            ),
+                                          ),
+                                          backgroundColor: kWhiteColor,
+                                        ),
+                                      )
+                                    : ElevatedButton(
+                                        onPressed: () {
+                                          EasyLoading.showError(
+                                            'Masukan file terlebihi dahulu',
+                                          );
+                                        },
+                                        child: Text(
+                                          'Lihat',
+                                          style: blackTextStyle.copyWith(
+                                            fontSize: 16.sp,
+                                            fontWeight: medium,
+                                          ),
+                                        ),
+                                        style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(7),
+                                            side: BorderSide(
+                                              color: kGreyColor,
+                                            ),
+                                          ),
+                                          backgroundColor: kWhiteColor,
+                                        ));
+                              },
+                            ),
+                          ),
+                          Container(
+                            width: 120.w,
+                            height: 40.h,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                controller.selectImageKK();
+                              },
+                              child: Text(
+                                'Pilih File',
+                                style: blackTextStyle.copyWith(
+                                  fontSize: 16.sp,
+                                  fontWeight: medium,
+                                ),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(7),
+                                  side: BorderSide(
+                                    color: kGreyColor,
+                                  ),
+                                ),
+                                backgroundColor: kWhiteColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
