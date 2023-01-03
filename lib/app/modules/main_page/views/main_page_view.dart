ba2469bf9861a03460814ea43c3ce8deeca9787a
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2023-01-03 10:54:32 +0800

    update

--- lib/app/modules/main_page/views/main_page_view.dart
+++ lib/app/modules/main_page/views/main_page_view.dart
@@ -1,14 +1,9 @@
-import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:get/get.dart';
 import 'package:pemohon_dukcapil_app/app/modules/history/views/history_view.dart';
-import 'package:pemohon_dukcapil_app/app/modules/profile/views/profile_view.dart';
+import 'package:pemohon_dukcapil_app/app/modules/home/views/home_view.dart';
 import 'package:pemohon_dukcapil_app/app/modules/settings/views/settings_view.dart';
-
-import '../../../utils/bottom_bar.dart';
-import '../../home/views/home_view.dart';
 import '../controllers/main_page_controller.dart';
 
 class MainPageView extends GetView<MainPageController> {
@@ -16,84 +11,27 @@ class MainPageView extends GetView<MainPageController> {
   Widget build(BuildContext context) {
     ScreenUtil.init(context, designSize: const Size(360, 690));
     return Scaffold(
-      body: Obx(
-        () => Stack(
+      body: SizedBox.expand(
+        child: PageView(
+          physics: ScrollPhysics(
+            parent: NeverScrollableScrollPhysics(),
+          ),
           children: [
-            controller.selected.value == ButtonIcons.home
-                ? HomeView()
-                : Container(),
-            controller.selected.value == ButtonIcons.history
-                ? HistoryView()
-                : Container(),
-            controller.selected.value == ButtonIcons.profile
-                ? ProfileView()
-                : Container(),
-            controller.selected.value == ButtonIcons.settings
-                ? SettingsView()
-                : Container(),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Material(
-                elevation: 20,
-                child: Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.only(
-                      left: 24.w, right: 24.w, bottom: 20.h, top: 20.h),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      /// HOME
-                      BottomBar(
-                        onPressed: () => controller.homeButton(),
-                        bottomIcons:
-                            controller.selected.value == ButtonIcons.home
-                                ? true
-                                : false,
-                        icons: EvaIcons.home,
-                        text: "Beranda",
-                        icons2: EvaIcons.home,
-                      ),
-
-                      /// HISTORY
-                      BottomBar(
-                        onPressed: () => controller.historyButton(),
-                        bottomIcons:
-                            controller.selected.value == ButtonIcons.history
-                                ? true
-                                : false,
-                        icons: EvaIcons.fileTextOutline,
-                        text: 'Riwayat',
-                        icons2: EvaIcons.fileTextOutline,
-                      ),
-
-                      /// PROFILE
-                      BottomBar(
-                        onPressed: () => controller.chatButton(),
-                        bottomIcons:
-                            controller.selected.value == ButtonIcons.profile
-                                ? true
-                                : false,
-                        icons: EvaIcons.personOutline,
-                        text: 'Profil',
-                        icons2: EvaIcons.personOutline,
-                      ),
-
-                      /// SETTINGS
-                      BottomBar(
-                        onPressed: () => controller.settingsButton(),
-                        bottomIcons:
-                            controller.selected.value == ButtonIcons.settings
-                                ? true
-                                : false,
-                        icons: EvaIcons.settingsOutline,
-                        text: 'Pengaturan',
-                        icons2: EvaIcons.settingsOutline,
-                      ),
-                    ],
-                  ),
-                ),
+            Container(
+              child: Center(
+                child: HomeView(),
               ),
-            )
+            ),
+            Container(
+              child: Center(
+                child: HistoryView(),
+              ),
+            ),
+            Container(
+              child: Center(
+                child: SettingsView(),
+              ),
+            ),
           ],
         ),
       ),
