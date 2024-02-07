734e1a94125e68414034cd9dbba1bda633f349bb
Christ Henry Loha <cristloha2205@gmail.com>
2024-02-07 09:51:19 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'app/routes/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'app/utils/error_screen.dart';
+import 'app/utils/splash_screen.dart';
+import 'firebase_options.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
